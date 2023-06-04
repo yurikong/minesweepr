1885a302152e7d5ec4717856a765f6b156f5c1a7
Jianqiang Du <jackdu2013@163.com>
2023-06-04 21:53:45 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath, URL } from "node:url"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
+  publicDir: "public",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets"
+  },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
